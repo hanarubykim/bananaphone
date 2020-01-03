54d9761fc0d76ff3df8523e917ffbe369811e1a5
Hana Kim <hanarubykim2002@gmail.com>
2020-01-03 00:25:45 -0500

    finished story writing?

--- story.c
+++ story.c
@@ -1,14 +1,31 @@
 #include <stdlib.h>
-// Will attempt to access the resources using the semaphore
-// Once in, it should display the last line added to the file
-// (use shared memory to contain the size of the last line).
-// Prompt the use for the next line.
-// Once a new line is read, write that to the file,
-//update the shared memory and then release the semaphore
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys.ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <fnctl.h>
 
+#define KEY 24602
+
+union semun {
+  int              val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                              (Linux-specific) */
+};
 
 int main(){
   //Use semaphore
+  int desc = semget(KEY, 1, 0);
+  struct sembuf* s = calloc(sizeof(struct sembuf), 1);
+  s->sem_num = 0;
+  s->sem_op = -1;
+  s->sem_flg = SEM_UNDO;
+  int current_stat = semop(desc, s, 1);
+  int shmid = shmget(KEY, sizeof(int), 0644);
 
   int fd = open("a.txt", O_RDWR | O_APPEND);
   char * current = calloc(10000, sizeof(char));
@@ -16,6 +33,10 @@ int main(){
 
   // Display last line added to the file
   // Use shared memory to contain size of last line
+  int * d = shmat(shmid, 0, 0);
+  int length = d[0];
+  char * lastLine = current + strlen(current) - length;
+  printf("The last line: %s\n", lastLine);
 
   // Prompt use for the next line
   printf("Add a new line: \n");
@@ -26,7 +47,13 @@ int main(){
 
   // Write that to the file
   write(fd, toAdd, nextLineLen);
+
   // Update the shared memory
+  d[0] = nextLineLength;
 
   // Release the semaphore
+  s->sem_op = 1;
+  semop(desc, s, 1);
+
+  return 0;
 }
